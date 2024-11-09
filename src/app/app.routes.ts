3d9ef02988e995843af0ba6b0866c1d84c8c7a33
Aitooor <aitorarias19@gmail.com>
2024-11-09 04:46:38 +0100

    feat: added sample error page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { ContactComponent } from './contact/contact.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { AboutComponent } from './about/about.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { ErrorComponent } from './error/error.component';
 
 export const routes: Routes = [
   { path: 'contact', component: ContactComponent },
   { path: 'projects', component: ProjectsComponent },
   { path: 'about', component: AboutComponent },
   { path: '', component: MainPageComponent }, // Main page
-  { path: '**', component: MainPageComponent } // Wildcard route for a 404 page
+  { path: '**', component: ErrorComponent } // Wildcard route for a 404 page
 ];
