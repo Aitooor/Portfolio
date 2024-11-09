abafaf3f41acddb1adedfa89d58662d7af0e6903
Aitooor <aitorarias19@gmail.com>
2024-11-09 04:26:02 +0100

    feat: use at default app.component header-visibility.service.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,25 @@
+// src/app/app.component.ts
 import { Component } from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
-import {HeaderComponent} from './header/header.component';
-import {FooterComponent} from './footer/footer.component';
+import { RouterOutlet } from '@angular/router';
+import { HeaderVisibilityService } from './header/service/header-visibility.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, HeaderComponent, FooterComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Aitor Arias Benéitez';
+  showHeader = true;
+
+  constructor(private headerVisibilityService: HeaderVisibilityService) {
+    this.headerVisibilityService.showHeader$.subscribe(show => {
+      this.showHeader = show;
+    });
+  }
 }
