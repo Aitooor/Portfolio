5d6054591b7b4ccc5aeb07025e8816e64b3663ab
Aitooor <aitorarias19@gmail.com>
2024-11-09 21:13:31 +0100

    feat: loading-screen.component created

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ProjectsService, Project } from '../projects/services/projects.service';
 import { HeaderVisibilityService } from '../header/service/header-visibility.service';
+import {LoadingScreenComponent} from '../loading-screen/loading-screen.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, LoadingScreenComponent],
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css']
 })
@@ -15,6 +16,7 @@ export class MainPageComponent implements OnInit, OnDestroy {
   projects: Project[] = [];
   startIndex = 0;
   limit = 5;
+  isLoading = true;
 
   constructor(
     private projectService: ProjectsService,
@@ -25,6 +27,7 @@ export class MainPageComponent implements OnInit, OnDestroy {
     this.headerVisibilityService.setShowHeader(false);
     this.projectService.getProjects(this.startIndex, this.limit).subscribe((projects) => {
       this.projects = projects;
+      this.isLoading = false;
     });
   }
 
