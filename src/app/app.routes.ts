7752eeb60ea99937863c8deeda1d650d60e072a0
Aitooor <aitorarias19@gmail.com>
2024-11-09 03:01:29 +0100

    feat: improve header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,13 @@
 import { Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
 import { MainPageComponent } from './main-page/main-page.component';
 
 export const routes: Routes = [
-  { path: 'about', component: AboutComponent },
-  { path: 'projects', component: ProjectsComponent },
   { path: 'contact', component: ContactComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'about', component: AboutComponent },
   { path: '', component: MainPageComponent }, // Main page
   { path: '**', component: MainPageComponent } // Wildcard route for a 404 page
 ];
