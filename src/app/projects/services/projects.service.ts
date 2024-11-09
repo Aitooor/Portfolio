aa28f41889dd568bfcae17cf6c7bdba350039d9f
Aitooor <aitorarias19@gmail.com>
2024-11-09 02:09:36 +0100

    feat: added projects

--- src/app/projects/services/projects.service.ts
+++ src/app/projects/services/projects.service.ts
@@ -1,13 +1,14 @@
 // src/app/projects/services/projects.service.ts
-import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { delay } from 'rxjs/operators';
+import {Injectable} from '@angular/core';
+import {Observable, of} from 'rxjs';
+import {delay} from 'rxjs/operators';
 
 export interface Project {
   id: number;
   title: string;
   description: string;
   image: string;
+  technologies?: string[];
 }
 
 @Injectable({
@@ -15,21 +16,9 @@ export interface Project {
 })
 export class ProjectsService {
   private projects: Project[] = [
-    { id: 1, title: 'Ignis', description: 'CEO of Hispanic Minecraft server(not affiliated with Microsoft/Mojang).', image: 'assets/images/project1.jpg' },
-    { id: 2, title: 'Nasgar', description: 'CEO of Multi language Minecraft server(not affiliated with Microsoft/Mojang).', image: 'assets/images/project1.jpg' },
-    { id: 3, title: 'Project Three', description: 'Description for project three.', image: 'assets/images/project1.jpg' },
-    { id: 4, title: 'Project Fourth', description: 'Description for project four.', image: 'assets/images/project1.jpg' },
-    { id: 5, title: 'Project Five', description: 'Description for project five.', image: 'assets/images/project1.jpg' },
-    { id: 6, title: 'Project Six', description: 'Description for project six.', image: 'assets/images/project1.jpg' },
-    { id: 7, title: 'Project Seven', description: 'Description for project seven.', image: 'assets/images/project1.jpg' },
-    { id: 8, title: 'Project Eight', description: 'Description for project eight.', image: 'assets/images/project1.jpg' },
-    { id: 9, title: 'Project Nine', description: 'Description for project nine.', image: 'assets/images/project1.jpg' },
-    { id: 10, title: 'Project Ten', description: 'Description for project ten.', image: 'assets/images/project1.jpg' },
-    { id: 11, title: 'Project Eleven', description: 'Description for project eleven.', image: 'assets/images/project1.jpg' },
-    { id: 12, title: 'Project Twelve', description: 'Description for project twelve.', image: 'assets/images/project1.jpg' },
-    { id: 13, title: 'Project Thirteen', description: 'Description for project thirteen.', image: 'assets/images/project1.jpg' },
-    { id: 14, title: 'Project Fourteen', description: 'Description for project fourteen.', image: 'assets/images/project1.jpg' },
-    { id: 15, title: 'Project Fifteen', description: 'Description for project fifteen.', image: 'assets/images/project1.jpg' },
+    { id: 1, title: 'Ignis', description: 'CEO of Hispanic Minecraft server(not affiliated with Microsoft/Mojang).', image: 'assets/images/project1.jpg', technologies: ['Java', 'Minecraft', 'SpigotAPI', 'PapermcAPI', 'VelocityAPI'] },
+    { id: 2, title: 'Nasgar', description: 'CEO of Multi language Minecraft server(not affiliated with Microsoft/Mojang).', image: 'assets/images/project1.jpg', technologies: ['Java', 'Minecraft', 'SpigotAPI', 'PapermcAPI', 'VelocityAPI'] },
+    { id: 3, title: 'This Portafolio', description: 'Simple Front-end to see my Back-end proyects.', image: 'assets/images/project1.jpg', technologies: ['HTML', 'CSS', 'TypeScript', 'Angular'] },
   ];
 
   getProjects(startIndex: number, limit: number): Observable<Project[]> {
